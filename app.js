af4fba5fb119dd627fddf26378f223fe4af570ad
Anurag Adhikari <anuragadhikari5276@gmail.com>
2024-10-13 00:10:10 +0545

    routes and models

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const userRouter = require('./modules/users/user.routes');
+require('dotenv').config();
+
+const PORT = process.env.PORT;
+const db_conn = process.env.db_conn;
+
+const app = express();
+app.use(express.json());
+
+//Models...
+require('./models/user.model');
+
+mongoose
+    .connect(db_conn, {})
+    .then(() => {
+        console.log('Database connection successful!');
+    })
+    .catch((err) => {
+        console.log('Database connection failed!', err);
+    });
+
+//Routes...
+app.use('/api/user', userRouter);
+
+app.listen(PORT, () => {
+    console.log(`Server started at port: ${PORT}`);
+});
---
+++ models/user.model.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: [true, 'Name cannot be empty'],
+        },
+        username: {
+            type: String,
+            required: [true, 'Username cannot be empty'],
+            unique: true,
+        },
+        email: {
+            type: String,
+            required: [true, 'Email cannot be empty'],
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: [true, 'Password cannot be empty'],
+        },
+        address: {
+            type: String,
+        },
+        balance: {
+            type: Number,
+            required: [true, 'Balance cannot be empty'],
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+const userModel = mongoose.model('users', userSchema);
+
+module.exports = userModel;
