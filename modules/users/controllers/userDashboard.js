2def8a6a9f112469d9c4dce38d78e37af72b9dd1
Anurag Adhikari <anuragadhikari5276@gmail.com>
2024-10-15 02:09:03 +0545

    user dashboard done

--- modules/users/controllers/userDashboard.js
+++ modules/users/controllers/userDashboard.js
@@ -2,15 +2,35 @@ const mongoose = require('mongoose');
 
 const userDashboard = async (req, res) => {
     const user = mongoose.model('users');
+    const Transaction = mongoose.model('transactions');
 
-    const getUserData = await user
-        .findOne({
-            _id: req.user._id,
+    let getTransactionHistory;
+    let getUserData;
+
+    try {
+        getTransactionHistory = await Transaction.find({
+            user_id: req.user._id,
         })
-        .select('balance name');
+            .select('amount remarks transaction_type createdAt')
+            .sort('-createdAt')
+            .limit(5);
+
+        getUserData = await user
+            .findOne({
+                _id: req.user._id,
+            })
+            .select('balance name');
+    } catch (err) {
+        res.status(400).json({
+            status: 'Failed',
+            message: err.message,
+        });
+    }
+
     // console.log(req.user.email);
     res.status(200).json({
         data: getUserData,
+        transactions: getTransactionHistory,
     });
 };
 
